457007d4a458f437bfc9076da51fcf36f0b46170
Alexander Pereira <xanderpereiraj@gmail.com>
2023-05-07 16:34:35 -0700

    :construction: Got base of comment system working with display and comment ability

--- components/main/Comment.js
+++ components/main/Comment.js
@@ -2,20 +2,18 @@ import React, { useState, useEffect} from 'react'
 import { View, Text, FlatList, Button, TextInput } from 'react-native'
 
 import { getAuth } from 'firebase/auth';
-import { getFirestore, collection, doc, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, doc, getDocs, addDoc } from 'firebase/firestore';
 
 export default function Comment(props) {
-  const [comments, setComments]  = useState([])
-  const [postId, setPostId] = useState("")
-  const [text, setText] = useState("")
+  const [comments, setComments] = useState([]);
+  const [postId, setPostId] = useState('');
+  const [text, setText] = useState('');
 
   useEffect(() => {
     const fetchComments = async () => {
       if (props.route.params.postId !== postId) {
         const db = getFirestore();
-
-        const commentsRef = collection(doc(db,'posts',props.route.params.uid,'userPosts',props.route.params.postId,'comments'));
-
+        const commentsRef = collection(db,'posts',props.route.params.uid,'userPosts',props.route.params.postId,'comments');
         const querySnapshot = await getDocs(commentsRef);
         const commentsData = querySnapshot.docs.map((doc) => {
           const data = doc.data();
@@ -27,20 +25,40 @@ export default function Comment(props) {
       }
     };
     fetchComments();
-  }, [props.route.params.postId])
+  }, [props.route.params.postId]);
+
+  const onCommentSend = async () => {
+    const auth = getAuth();
+    const db = getFirestore();
+
+    await addDoc(collection(db,'posts',props.route.params.uid,'userPosts',props.route.params.postId,'comments'),
+      {
+        creator: auth.currentUser.uid,
+        text: text,
+      }
+    );
+    setText('');
+  };
 
   return (
     <View>
-      <FlatList>
-        numColumns = {1}
-        horizontal = {false}
-        data = {comments}
-        renderItem = {({item}) => (
+      <FlatList
+        numColumns={1}
+        horizontal={false}
+        data={comments}
+        renderItem={({ item }) => (
           <View>
             <Text>{item.text}</Text>
           </View>
         )}
-      </FlatList>
+      />
+      <View>
+        <TextInput
+          placeholder="comment..."
+          onChangeText={(text) => setText(text)}
+        />
+        <Button onPress={onCommentSend} title="Send" />
+      </View>
     </View>
   );
 }
